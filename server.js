42859da182acba3ef807c36061deba22f826196b
owenzel <oliviacwenzel@gmail.com>
2020-11-25 11:55:25 -0500

    More basic set up and testing with the server

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-const express = require('express');
+const express = require('express'); // a framework for better handling http requests & responses
+const { body, validationResult } = require('express-validator'); //set of middlewares that will help clean up user input
 const app = express();
 
-// Test route
+app.use(express.json());
+
+// Test route:
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+// Test route:
+app.post('/test', [body('text').not().isEmpty().trim()], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
